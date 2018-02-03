11a0569e916092c5f307be46be5dccf43c81b08e
shineSnow <1358205616@qq.com>
2018-02-03 10:45:22 +0800

    Add .babelrc babel-preset-react

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# webpack-开发
+# webpack-react-开发
 
-##实现webpack hmr 功能
-
-参考链接
-*['http://blog.csdn.net/binggoogle/article/details/53105328]
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import 'normalize.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 if(module.hot) { module.hot.accept()}
 
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
